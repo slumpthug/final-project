9548d06cff6588911af8b2b706c8ce364cf5f735
slumpthug <gangguchi58@gmail.com>
2022-12-08 15:07:20 +0600

    added footer and styles to it

--- src/pages/content/Content.jsx
+++ src/pages/content/Content.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../../components/footer/Footer';
 import Header from '../../components/header/Header';
 
 const Content = () => {
     return (
         <div className='Content'>
             <Header/>
+            <Footer/>
         </div>
     );
 };
