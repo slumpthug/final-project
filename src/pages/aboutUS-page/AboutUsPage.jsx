87048db0c3535abd139544530c8ddc22dba31230
slumpthug <gangguchi58@gmail.com>
2022-12-08 15:38:28 +0600

    corrected footer location

--- src/pages/aboutUS-page/AboutUsPage.jsx
+++ src/pages/aboutUS-page/AboutUsPage.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import AboutClub from '../../components/aboutClub/AboutClub';
-import Footer from '../../components/footer/Footer'
 
 const Aboutus = () => {
     return (
         <div className='Aboutus'>  
             <AboutClub/>
-            <Footer/>
         </div>
     );
 };
--- src/pages/content/Content.jsx
+++ src/pages/content/Content.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import Header from '../../components/header/Header';
 import AboutUs from '../aboutUS-page/AboutUsPage';
+import Footer from '../../components/footer/Footer'
 
 const Content = () => {
     return (
@@ -11,6 +12,7 @@ const Content = () => {
                 <Route path='/' element={ <AboutUs/> }/>
                 <Route path='/aboutus' element={ <AboutUs/> } />
             </Routes>
+            <Footer/>
         </div>
     );
 };
