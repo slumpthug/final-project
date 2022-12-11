f0fef3525ec35029e20d8ea22aaabf136d52229a
slumpthug <gangguchi58@gmail.com>
2022-12-11 22:19:38 +0600

    added requests in computers modal

--- src/components/computersModalBlock/ComputersModalBlock.jsx
+++ src/components/computersModalBlock/ComputersModalBlock.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './ComputersModalBlock-style.css';
+import axios from 'axios';
 
 const ComputersModalBlock = ({activeModal, setActiveModal}) => {
+    const [computers, setComputers] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get('http://roywest3.pythonanywhere.com/api/v1/computer-list/')
+            .then (data => {
+                setComputers(data.data)
+            })
+    }, [])
+
     return (
         <div className={activeModal ? 'ComputersModalBlock activeModal' : 'ComputersModalBlock'} onClick={() => setActiveModal(false)}>
             <div className={activeModal ? 'ComputersModalBlock__content activeModal__content' : 'ComputersModalBlock__content'} onClick={(e) => e.stopPropagation()}>
-                hello
+                <div className="computer__range">
+                    { computers.filter(computer => computer.room == ('1')).map(computer => {
+                        return (
+                            <>
+
+                                <div className='computer'></div>
+                                
+                            </>
+                        )
+                    })}
+                </div>
+                { computers.filter(computer => computer.id == ('113')).map(computer => {
+                                    return (
+                                        <>
+
+                                            <div className='computers__desc'>{computer.additional_description}</div>
+
+                                        </>
+                                    )
+                })}
             </div>
         </div>
     );
