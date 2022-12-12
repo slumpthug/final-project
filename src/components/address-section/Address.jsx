cb6fc9eed6667b93e5c8afd05cdc4ebed7ee475a
slumpthug <gangguchi58@gmail.com>
2022-12-12 20:45:45 +0600

    added requests in address section

--- src/components/address-section/Address.jsx
+++ src/components/address-section/Address.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Address-style.css';
+import axios from 'axios';
 import { motion } from 'framer-motion';
 
 const textAnimation = {
@@ -15,6 +16,17 @@ const textAnimation = {
 }
 
 const Address = () => {
+    const [address, setAddress] = useState([])
+
+    useEffect(() => {
+        axios
+            .get('http://roywest3.pythonanywhere.com/api/v1/address-list/')
+            .then (data => {
+                setAddress(data.data)
+            })
+    }, [])
+
+
     return (
         <motion.div
          initial='hidden'
@@ -22,98 +34,31 @@ const Address = () => {
          viewport={{ amount: .2, once: true }}
          className='Address'
         >
-            <div className="Address__card-range">
-                <motion.div variants={textAnimation} custom={1} className="Address__card">
-                    <a className='Address__card-title' href="#">Байтик Баатыра 96</a>
-                    <h4 className='Address__card-subtitle'>ул. Байтик Баатыра (Советская), 96</h4>
-                    <div className='Address__card-item'>
-                        <span>
-                            Работа компьтерного зала:
-                            <div>24 часа</div>
-                        </span>
-                        <span>
-                            Работа food зоны:
-                            <div>с 08:00 до 23:00</div>
-                        </span>
-                        <span>
-                            Рабочий номер:
-                            <div>+996 555 056 948</div>
-                        </span>
-                    </div>
-                </motion.div>
-                <motion.div variants={textAnimation} custom={2} className="Address__card">
-                    <a className='Address__card-title' href="#">Байтик Баатыра 96</a>
-                    <h4 className='Address__card-subtitle'>ул. Байтик Баатыра (Советская), 96</h4>
-                    <div className='Address__card-item'>
-                        <span>
-                            Работа компьтерного зала:
-                            <div>24 часа</div>
-                        </span>
-                        <span>
-                            Работа food зоны:
-                            <div>с 08:00 до 23:00</div>
-                        </span>
-                        <span>
-                            Рабочий номер:
-                            <div>+996 555 056 948</div>
-                        </span>
-                    </div>
-                </motion.div>
-                <motion.div variants={textAnimation} custom={3} className="Address__card">
-                    <a className='Address__card-title' href="#">Байтик Баатыра 96</a>
-                    <h4 className='Address__card-subtitle'>ул. Байтик Баатыра (Советская), 96</h4>
-                    <div className='Address__card-item'>
-                        <span>
-                            Работа компьтерного зала:
-                            <div>24 часа</div>
-                        </span>
-                        <span>
-                            Работа food зоны:
-                            <div>с 08:00 до 23:00</div>
-                        </span>
-                        <span>
-                            Рабочий номер:
-                            <div>+996 555 056 948</div>
-                        </span>
-                    </div>
-                </motion.div>
-                <motion.div variants={textAnimation} custom={4} className="Address__card">
-                    <a className='Address__card-title' href="#">Байтик Баатыра 96</a>
-                    <h4 className='Address__card-subtitle'>ул. Байтик Баатыра (Советская), 96</h4>
-                    <div className='Address__card-item'>
-                        <span>
-                            Работа компьтерного зала:
-                            <div>24 часа</div>
-                        </span>
-                        <span>
-                            Работа food зоны:
-                            <div>с 08:00 до 23:00</div>
-                        </span>
-                        <span>
-                            Рабочий номер:
-                            <div>+996 555 056 948</div>
-                        </span>
-                    </div>
-                </motion.div>
-                <motion.div variants={textAnimation} custom={5} className="Address__card">
-                    <a className='Address__card-title' href="#">Байтик Баатыра 96</a>
-                    <h4 className='Address__card-subtitle'>ул. Байтик Баатыра (Советская), 96</h4>
-                    <div className='Address__card-item'>
-                        <span>
-                            Работа компьтерного зала:
-                            <div>24 часа</div>
-                        </span>
-                        <span>
-                            Работа food зоны:
-                            <div>с 08:00 до 23:00</div>
-                        </span>
-                        <span>
-                            Рабочий номер:
-                            <div>+996 555 056 948</div>
-                        </span>
-                    </div>
-                </motion.div>
-            </div>
+            <motion.div variants={textAnimation} custom={1} className="Address__card-range">
+                { address.map(addr => {
+                    return (
+                        <>
+                            <div  className="Address__card">
+                                <a className='Address__card-title' href={addr.address_link}>{addr.address}</a>
+                                <div className='Address__card-item'>
+                                    <span>
+                                        Работа компьтерного зала:
+                                        <div>{addr.working_hours}</div>
+                                    </span>
+                                    <span>
+                                        Работа food зоны:
+                                        <div>{addr.foodzone_working_hours}</div>
+                                    </span>
+                                    <span>
+                                        Рабочий номер:
+                                        <div>+996 555 056 948</div>
+                                    </span>
+                                </div>
+                            </div>
+                        </>
+                    )
+                })}
+            </motion.div>
         </motion.div>
     );
 };
