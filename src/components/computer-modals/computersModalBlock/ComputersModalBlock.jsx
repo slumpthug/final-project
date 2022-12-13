6627aa509b457e45486c5e5ee0e601096a91258b
slumpthug <gangguchi58@gmail.com>
2022-12-13 13:08:12 +0600

    added styles for modals

--- src/components/computer-modals/computersModalBlock/ComputersModalBlock.jsx
+++ src/components/computer-modals/computersModalBlock/ComputersModalBlock.jsx
@@ -17,16 +17,41 @@ const ComputersModalBlock = ({activeModal, setActiveModal}) => {
     return (
         <div className={activeModal ? 'ComputersModalBlock activeModal' : 'ComputersModalBlock'} onClick={() => setActiveModal(false)}>
             <div className={activeModal ? 'ComputersModalBlock__content activeModal__content' : 'ComputersModalBlock__content'} onClick={(e) => e.stopPropagation()}>
-                <div className="computer__range">
-                    { computers.filter(computer => computer.room == ('1')).map(computer => {
-                        return (
-                            <>
+                <div className="ComputersModalBlock__content-left">
+                    <h1 className='ComputersModalBlock__title'>ОБЩИЙ ЗАЛ</h1>
+                    <div className="computers-rate__range">
+                        <div className="computer-rate__cont">
+                            <div className="computer"></div>
+                            <span> - свободно</span>
+                        </div>
+                        <div className="computer-rate__cont">
+                            <div className="computer computer_red"></div>
+                            <span> - занято</span>
+                        </div>
+                        <div className="computer-rate__cont">
+                            <div className="computer computer_busy"></div>
+                            <span> - ваша бронь</span>
+                        </div>
+                    </div>
+                    <div className="computer__range">
+                        { computers.filter(computer => computer.room == ('1')).map(computer => {
+                            return (
+                                <>
 
-                                <div /*className={(computer.is_busy == (false)) ? "computer computer_green" : "computer_red"}*/ className={`computer ${busy ? 'computer_busy' : ''}`} onClick={() => setBusy(busy => !busy)}></div>
-                                
-                            </>
-                        )
-                    })}
+                                    <div /*className={(computer.is_busy == (false)) ? "computer computer_green" : "computer_red"}*/ className={`computer ${busy ? 'computer_busy' : ''}`} onClick={() => setBusy(busy => !busy)}></div>
+
+                                </>
+                            )
+                        })}
+                    </div>
+                </div>
+                <div className="ComputersModalBlock__content-right">
+                    <h1>Внимание</h1>
+                    <ol>
+                        <li> - Вы можете забронировать не более 6 мест в сутки.</li>
+                        <li> - Невыкупленная бронь автоматически снимается за 10 минут.</li>
+                        <li> - В случае, если количество невыкупленных мест достигнет 3, вы будете заблокированы.</li>
+                    </ol>
                 </div>
             </div>
         </div>
--- src/components/computer-modals/computersModalBlockTwo/ComputersModalBlockTwo.jsx
+++ src/components/computer-modals/computersModalBlockTwo/ComputersModalBlockTwo.jsx
@@ -15,16 +15,41 @@ const ComputersModalBlockTwo = ({activeModalTwo, setActiveModalTwo}) => {
     return (
         <div className={activeModalTwo ? 'ComputersModalBlock activeModal' : 'ComputersModalBlock'} onClick={() => setActiveModalTwo(false)}>
             <div className={activeModalTwo ? 'ComputersModalBlock__content activeModal__content' : 'ComputersModalBlock__content'} onClick={(e) => e.stopPropagation()}>
-                <div className="computer__range">
-                    { computers.filter(computer => computer.room == ('2')).map(computer => {
-                        return (
-                            <>
+                <div className="ComputersModalBlock__content-left">
+                    <h1 className='ComputersModalBlock__title ComputersModalBlock__title_comfort'>КОМФОРТ ЗАЛ</h1>
+                    <div className="computers-rate__range">
+                        <div className="computer-rate__cont">
+                            <div className="computer"></div>
+                            <span> - свободно</span>
+                        </div>
+                        <div className="computer-rate__cont">
+                            <div className="computer computer_red"></div>
+                            <span> - занято</span>
+                        </div>
+                        <div className="computer-rate__cont">
+                            <div className="computer computer_busy"></div>
+                            <span> - ваша бронь</span>
+                        </div>
+                    </div>
+                    <div className="computer__range">
+                        { computers.filter(computer => computer.room == ('2')).map(computer => {
+                            return (
+                                <>
 
-                                <div className={(computer.is_busy == (false)) ? "computer computer_green" : "computer_red"}></div>
-                                
-                            </>
-                        )
-                    })}
+                                    <div className={(computer.is_busy == (false)) ? "computer computer_green" : "computer_red"}></div>
+
+                                </>
+                            )
+                        })}
+                    </div>
+                </div>
+                <div className="ComputersModalBlock__content-right">
+                    <h1>Внимание</h1>
+                    <ol>
+                        <li> - Вы можете забронировать не более 6 мест в сутки.</li>
+                        <li> - Невыкупленная бронь автоматически снимается за 10 минут.</li>
+                        <li> - В случае, если количество невыкупленных мест достигнет 3, вы будете заблокированы.</li>
+                    </ol>
                 </div>
             </div>
         </div>
